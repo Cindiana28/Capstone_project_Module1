356c7763e12ae9bebb0b7b9b36f55c7b89756e67
Cindiana28 <cindiadw@gmail.com>
2024-09-18 18:29:24 +0700

    update terbaru

--- CRUD_CINDI.py
+++ CRUD_CINDI.py
@@ -35,14 +35,15 @@ def show_menu():
         print("Kamu memilih menu yang salah!")
         back_to_menu()
 
-header = ["NIK", "NAMA", "UMUR", "GENDER", "PEKERJAAN", "JABATAN", "STATUS", "DOMISILI"]
-col_widths = [10, 15, 10, 15, 20, 10, 20, 15]
+header = ["NIK", "NAMA", "UMUR", "GENDER", "PEKERJAAN", "DOMISILI"]
+col_widths = [10, 15, 10, 15, 20, 10]
 
 def back_to_menu():
     print("\n")
     input("Tekan Enter untuk kembali...")
     show_menu()
 
+# READ
 
 def show_contact():
     clear_screen()
@@ -52,54 +53,45 @@ def show_contact():
         for row in csv_reader:
             contacts.append(row)
 
-    print(f'{header[0]:<{col_widths[0]}} {header[1]:<{col_widths[1]}} {header[2]:<{col_widths[2]}} {header[3]:<{col_widths[3]}} {header[4]:<{col_widths[4]}} {header[5]:<{col_widths[5]}} {header[6]:<{col_widths[6]}} {header[7]:<{col_widths[7]}}')
+    print(f'{header[0]:<{col_widths[0]}} {header[1]:<{col_widths[1]}} {header[2]:<{col_widths[2]}} {header[3]:<{col_widths[3]}} {header[4]:<{col_widths[4]}} {header[5]:<{col_widths[5]}}')
 
     print("-"*sum(col_widths))
     
     if (len(contacts) > 0):
         data = contacts.pop(0)
-        #print(f"{labels[0]} \t {labels[1]} \t {labels[2]} \t\t {labels[3]} \t\t {labels[4]} \t\t {labels[5]} \t\t {labels[6]} \t\t {labels[7]} \t\t {labels[8]}")
+        #print(f"{labels[0]} \t {labels[1]} \t {labels[2]} \t\t {labels[3]} \t\t {labels[4]} \t\t {labels[5]}")
         for data in contacts:
-            print(f'{data[0]:<{col_widths[0]}} {data[1]:<{col_widths[1]}} {data[2]:<{col_widths[2]}} {data[3]:<{col_widths[3]}} {data[4]:<{col_widths[4]}} {data[5]:<{col_widths[5]}} {data[6]:<{col_widths[6]}} {data[7]:<{col_widths[7]}}')
+            print(f'{data[0]:<{col_widths[0]}} {data[1]:<{col_widths[1]}} {data[2]:<{col_widths[2]}} {data[3]:<{col_widths[3]}} {data[4]:<{col_widths[4]}} {data[5]:<{col_widths[5]}}')
     else:
         print("Tidak ada data!")
     back_to_menu()
 
-def contains_all(value, array):
-    """
-    Memeriksa apakah nilai mengandung semua elemen dari array.
-
-    :param value: String yang akan diperiksa.
-    :param array: List dari string yang dicari.
-    :return: True jika nilai mengandung semua elemen dari array, False sebaliknya.
-    """
-    return all(element in value for element in array)
-
-
+        
 def create_contact():
     clear_screen()
     with open(csv_filename, mode='a', newline='') as csv_file:
-        fieldnames = ['NIK', 'NAMA','UMUR','GENDER','PEKERJAAN','JABATAN','STATUS','DOMISILI']
+        fieldnames = ['NIK', 'NAMA','UMUR','GENDER','PEKERJAAN','DOMISILI']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        
-        NIK = input("NIK: ").isnumeric()
+        NIK = input("NIK (Harus Angka): ")
+        if not NIK.isnumeric(): 
+            print('Data Harus Berbentuk angka, ulang kembali!')
+            back_to_menu()
         NAMA = input("Nama Lengkap: ")
         UMUR = input("Umur: ")
         GENDER = input("Jenis Kelamin: ")
-        PEKERJAAN = input("Nama Pekerjaan (HR, IT, MARKETING, OPRATION, BUSINESS, LEGAL): ")
-        listPekerjaan = ['HR','IT','MARKETING','OPRATION','BUSINESS','LEGAL']
-        isPekerjaanValid = contains_all(PEKERJAAN, listPekerjaan)
+        PEKERJAAN = input("Nama Pekerjaan (HR, IT, MARKETING, OPRATION, BUSINESS, LEGAL,FINANCE): ")
+        listPekerjaan = ['HR','IT','MARKETING','OPRATION','BUSINESS','LEGAL','FINANCE']
+        isPekerjaanValid = False
+        if PEKERJAAN in listPekerjaan:
+            isPekerjaanValid = True
+            
         if isPekerjaanValid: 
-            JABATAN = input("Jabatan (Officer, Supervisor, Manager, Vice President): ")
-            STATUS = input("Status Karyawan (KARYAWAN TETAP, KONTRAK, MAGANG): ")
             DOMISILI = input("Tempat Tinggal : ")
-            writer.writerow({'NIK': NIK,'NAMA': NAMA,'UMUR': UMUR,'GENDER' : GENDER,'PEKERJAAN' : PEKERJAAN,'JABATAN' : JABATAN,'STATUS' : STATUS,'DOMISILI' : DOMISILI})
+            writer.writerow({'NIK': NIK,'NAMA': NAMA,'UMUR': UMUR,'GENDER' : GENDER,'PEKERJAAN' : PEKERJAAN,'DOMISILI' : DOMISILI})
             print("Horeyyy Data Berhasil disimpan!")
             print("Benar")
-        else: print("Salah")
-        PEKERJAAN = input("Nama Pekerjaan (HR, IT, MARKETING, OPRATION, BUSINESS, LEGAL): ")
+        else: print("Salah! Silahkan input Kembali")
         
-
     back_to_menu()
 
 
@@ -131,11 +123,9 @@ def search_contact():
         print(f"UMUR: {data_found[2]}")
         print(f"GENDER: {data_found[3]}")
         print(f"PEKERJAAN: {data_found[4]}")
-        print(f"JABATAN: {data_found[5]}")
-        print(f"STATUS: {data_found[6]}")
-        print(f"DOMISILI: {data_found[7]}")
+        print(f"DOMISILI: {data_found[5]}")
     else:
-        print("Tidak ada data ditemukan")
+        print("Tidak ditemukan")
     back_to_menu()
     
 
@@ -149,22 +139,20 @@ def edit_contact():
         for row in csv_reader:
             contacts.append(row)
 
-    print(f'{header[0]:<{col_widths[0]}} {header[1]:<{col_widths[1]}} {header[2]:<{col_widths[2]}} {header[3]:<{col_widths[3]}} {header[4]:<{col_widths[4]}} {header[5]:<{col_widths[5]}} {header[6]:<{col_widths[6]}} {header[7]:<{col_widths[7]}}')
+    print(f'{header[0]:<{col_widths[0]}} {header[1]:<{col_widths[1]}} {header[2]:<{col_widths[2]}} {header[3]:<{col_widths[3]}} {header[4]:<{col_widths[4]}} {header[5]:<{col_widths[5]}}')
 
     print("-"*sum(col_widths))
 
     data = contacts.pop(0)
     for data in contacts:
-        print(f'{data[0]:<{col_widths[0]}} {data[1]:<{col_widths[1]}} {data[2]:<{col_widths[2]}} {data[3]:<{col_widths[3]}} {data[4]:<{col_widths[4]}} {data[5]:<{col_widths[5]}} {data[6]:<{col_widths[6]}} {data[7]:<{col_widths[7]}}')
+        print(f'{data[0]:<{col_widths[0]}} {data[1]:<{col_widths[1]}} {data[2]:<{col_widths[2]}} {data[3]:<{col_widths[3]}} {data[4]:<{col_widths[4]}} {data[5]:<{col_widths[5]}}')
 
     print("-----------------------")
     NIK = input("NIK: ")
     NAMA = input("Nama Lengkap: ")
     UMUR = input("Umur: ")
     GENDER = input("Jenis Kelamin: ")
-    PEKERJAAN = input("Nama Pekerjaan (HR, IT, MARKETING, OPRATION, BUSINESS, LEGAL): ")
-    JABATAN = input("Jabatan (Officer, Supervisor, Manager, Vice President): ")
-    STATUS = input("Status Karyawan (KARYAWAN TETAP, KONTRAK, MAGANG): ")
+    PEKERJAAN = input("Nama Pekerjaan (HR, IT, MARKETING, OPRATION, BUSINESS, LEGAL, FINANCE): ")
     DOMISILI = input("Tempat Tingga: ")
 
 
@@ -177,20 +165,17 @@ def edit_contact():
             contacts[indeks][2] = UMUR
             contacts[indeks][3] = GENDER
             contacts[indeks][4] = PEKERJAAN
-            contacts[indeks][5] = JABATAN
-            contacts[indeks][6] = STATUS
-            contacts[indeks][7] = DOMISILI
-
+            contacts[indeks][5] = DOMISILI
 
         indeks = indeks + 1
 
     # Menulis data baru ke file CSV (tulis ulang)
     with open(csv_filename, mode="w", newline='') as csv_file:
-        fieldnames = ['NIK', 'NAMA','UMUR','GENDER','PEKERJAAN','JABATAN','STATUS','DOMISILI']
+        fieldnames = ['NIK', 'NAMA','UMUR','GENDER','PEKERJAAN','DOMISILI']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         for new_data in contacts:
-            writer.writerow({'NIK': new_data[0], 'NAMA': new_data[1], 'UMUR': new_data[2], 'GENDER': new_data[3], 'PEKERJAAN': new_data[4], 'JABATAN': new_data[5], 'STATUS': new_data[6], 'DOMISILI': new_data[7]}) 
+            writer.writerow({'NIK': new_data[0], 'NAMA': new_data[1], 'UMUR': new_data[2], 'GENDER': new_data[3], 'PEKERJAAN': new_data[4], 'DOMISILI': new_data[5]}) 
 
     back_to_menu()
 
@@ -205,13 +190,13 @@ def delete_contact():
         for row in csv_reader:
             contacts.append(row)
 
-    print(f'{header[0]:<{col_widths[0]}} {header[1]:<{col_widths[1]}} {header[2]:<{col_widths[2]}} {header[3]:<{col_widths[3]}} {header[4]:<{col_widths[4]}} {header[5]:<{col_widths[5]}} {header[6]:<{col_widths[6]}} {header[7]:<{col_widths[7]}}')
+    print(f'{header[0]:<{col_widths[0]}} {header[1]:<{col_widths[1]}} {header[2]:<{col_widths[2]}} {header[3]:<{col_widths[3]}} {header[4]:<{col_widths[4]}} {header[5]:<{col_widths[5]}}')
 
     print("-"*sum(col_widths))
 
     data = contacts.pop(0)
     for data in contacts:
-        print(f'{data[0]:<{col_widths[0]}} {data[1]:<{col_widths[1]}} {data[2]:<{col_widths[2]}} {data[3]:<{col_widths[3]}} {data[4]:<{col_widths[4]}} {data[5]:<{col_widths[5]}} {data[6]:<{col_widths[6]}} {data[7]:<{col_widths[7]}}')
+        print(f'{data[0]:<{col_widths[0]}} {data[1]:<{col_widths[1]}} {data[2]:<{col_widths[2]}} {data[3]:<{col_widths[3]}} {data[4]:<{col_widths[4]}} {data[5]:<{col_widths[5]}}')
 
     print("-----------------------")
     NIK = input("Hapus NIK> ")
@@ -226,11 +211,11 @@ def delete_contact():
 
     # Menulis data baru ke file CSV (tulis ulang)
     with open(csv_filename, mode="w", newline='') as csv_file:
-        fieldnames = ['NIK', 'NAMA','UMUR','GENDER','PEKERJAAN','JABATAN','STATUS','DOMISILI']
+        fieldnames = ['NIK', 'NAMA','UMUR','GENDER','PEKERJAAN','DOMISILI']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         for new_data in contacts:
-            writer.writerow({'NIK': new_data[0], 'NAMA': new_data[1], 'UMUR': new_data[2], 'GENDER': new_data[3], 'PEKERJAAN': new_data[4], 'JABATAN': new_data[5], 'STATUS': new_data[6], 'DOMISILI': new_data[7]}) 
+            writer.writerow({'NIK': new_data[0], 'NAMA': new_data[1], 'UMUR': new_data[2], 'GENDER': new_data[3], 'PEKERJAAN': new_data[4],'DOMISILI': new_data[5]}) 
 
     print("Data sudah terhapus")
     back_to_menu()
@@ -238,3 +223,4 @@ def delete_contact():
 if __name__ == "__main__":
     while True:
         show_menu()
+
